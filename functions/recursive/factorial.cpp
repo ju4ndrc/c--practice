45f407ab965a13000b4c176d5ef080ead8dbcda4
Juan David <jdramirez90@ucatolica.edu.co>
2026-09-17 20:09:35 -0500

    Create factorial.cpp

---
+++ functions/recursive/factorial.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int factorial(int n){
+	
+	if(n < 1){
+		return 1;
+	}
+	return n * factorial(n - 1);
+	
+}
+
+int main(int argc, char** argv) {
+	int n;
+	cin>>n;
+	cout<<factorial(n);
+
+    return 0;
+}
+
+
+
+
